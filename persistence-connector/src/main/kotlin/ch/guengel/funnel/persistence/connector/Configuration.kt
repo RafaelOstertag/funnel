1f2fc2cec1b2c9d949552032ae665145bfd5ecdb
Rafael Ostertag <rafi@guengel.ch>
2018-12-02 22:52:03 +0100

    Specify lowercase config name so that reading from env works

--- persistence-connector/src/main/kotlin/ch/guengel/funnel/persistence/connector/Configuration.kt
+++ persistence-connector/src/main/kotlin/ch/guengel/funnel/persistence/connector/Configuration.kt
@@ -4,6 +4,6 @@ import com.uchuhimo.konf.ConfigSpec
 
 object Configuration : ConfigSpec("persistence.connector") {
     val kafka by optional("localhost:9092")
-    val mongoDbURL by optional("mongodb://localhost")
-    val mongoDb by optional("funnel")
+    val mongoDbURL by optional("mongodb://localhost", name = "mongodburl")
+    val mongoDb by optional("funnel", name = "mongodb")
 }
