b71f87d0326f704426a7757aa7f08162876f178b
Rafael Ostertag <rafi@guengel.ch>
2019-04-23 19:05:53 +0200

    Factor out embedded mongo to share between tests

---
+++ test-utils/src/main/kotlin/EmbeddedMongo.kt
@@ -0,0 +1,38 @@
+package ch.guengel.funnel.testutils
+
+import de.flapdoodle.embed.mongo.MongodExecutable
+import de.flapdoodle.embed.mongo.MongodProcess
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.runtime.Network
+
+class EmbeddedMongo {
+    val mongoPort = Network.getFreeServerPort()
+
+    private var mongodConfig = MongodConfigBuilder()
+        .version(Version.Main.PRODUCTION)
+        .net(Net(mongoPort, Network.localhostIsIPv6()))
+        .build()
+    private val mongoServer: MongodExecutable = MongodStarter.getDefaultInstance().prepare(mongodConfig)
+    private var mongod: MongodProcess? = null
+
+    fun start() {
+        if (mongod != null) {
+            System.err.println("Warning: Embedded mongo already started on port ${mongoPort}")
+            return
+        }
+
+        mongod = mongoServer.start()
+    }
+
+    fun stop() {
+        if (mongod == null) {
+            System.err.println("Warning: Embedded mongo not started. Call start() first")
+            return
+        }
+        mongod?.stop()
+        mongod = null
+    }
+}
