0e951ee4bb395f82dc4d9e626213fae081f3614f
Rafael Ostertag <rafi@guengel.ch>
2019-04-23 19:16:37 +0200

    Use embedded mongodb from test utils

--- persistence-connector/src/test/kotlin/ch/guengel/funnel/persistence/connector/PersistenceConnectorIT.kt
+++ persistence-connector/src/test/kotlin/ch/guengel/funnel/persistence/connector/PersistenceConnectorIT.kt
@@ -11,26 +11,49 @@ import ch.guengel.funnel.kafka.Consumer
 import ch.guengel.funnel.kafka.Producer
 import ch.guengel.funnel.kafka.Topics
 import ch.guengel.funnel.readConfiguration
+import ch.guengel.funnel.testutils.EmbeddedMongo
+import com.uchuhimo.konf.Config
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import makeFeed
 import makeFeedEnvelope
 import makeSource
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 
-
+/**
+ * This test requires a running Kafka locally
+ */
 @EnabledIfEnvironmentVariable(named = "HAS_LOCAL_ENVIRONMENT", matches = "yes|true")
 class PersistenceConnectorIT {
-    private var configuration = readConfiguration(Configuration)
+    private var embeddedMongo: EmbeddedMongo? = null
+
+    // Will be overriden by setUp(). But instead of making it nullable, we go with this and guarantee that it is not null
+    private var configuration: Config = readConfiguration(Configuration)
+
+    @BeforeEach
+    fun setUp() {
+        embeddedMongo = EmbeddedMongo()
+        embeddedMongo?.start()
+        System.getProperties()
+            .setProperty("persistence.connector.mongodburl", "mongodb://localhost:${embeddedMongo?.mongoPort}")
+        configuration = readConfiguration(Configuration)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        embeddedMongo?.stop()
+    }
 
     @Test
     fun `end to end`() {
-        var testSuccess = false
         val feedEnvelope = makeFeedEnvelope(makeSource(1), makeFeed("testId", "test title 1", 9))
 
         // The listener to all retrieved feeds
         val consumer = setupListOfFeedEnvelopesConsumer()
+        var testSuccess = false
         consumer.start { topic, key, data ->
             if (topic == Topics.retrieveAll &&
                 key == "the-test-key" &&
@@ -51,6 +74,7 @@ class PersistenceConnectorIT {
         val testConsumer = setUpConsumer(configuration)
         topicHandler.use {
             testConsumer.start(it::handle)
+
 
             sendSaveEnvelopeMessage(feedEnvelope)
 
