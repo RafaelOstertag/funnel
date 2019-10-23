2881a6cb0b6fdcd7b7eba423febc998e1b2b406b
rafi@guengel.ch <rafi@guengel.ch>
2019-10-23 18:07:26 +0200

    FeedEnvelopeSaver does not depend on FeedEnvelopeMerger

--- feed/src/main/kotlin/logic/FeedEnvelopeSaver.kt
+++ feed/src/main/kotlin/logic/FeedEnvelopeSaver.kt
@@ -5,16 +5,10 @@ import data.FeedEnvelope
 
 class FeedEnvelopeSaver(
     private val feedPersistence: FeedPersistence,
-    private val feedEnvelopeTrimmer: FeedEnvelopeTrimmer,
-    private val feedEnvelopeMerger: FeedEnvelopeMerger
+    private val feedEnvelopeTrimmer: FeedEnvelopeTrimmer
 ) {
     fun save(feedEnvelope: FeedEnvelope) {
         val trimmedFeed = feedEnvelopeTrimmer.trim(feedEnvelope)
         feedPersistence.saveFeedEnvelope(trimmedFeed)
-    }
-
-    fun mergeAndSave(feedEnvelope1: FeedEnvelope, feedEnvelope2: FeedEnvelope) {
-        val mergedFeedEnvelope = feedEnvelopeMerger.merge(feedEnvelope1, feedEnvelope2)
-        save(mergedFeedEnvelope)
     }
 }
--- feed/src/test/kotlin/logic/FeedEnvelopeSaverTest.kt
+++ feed/src/test/kotlin/logic/FeedEnvelopeSaverTest.kt
@@ -18,37 +18,14 @@ internal class FeedEnvelopeSaverTest {
         every { persistenceMock.saveFeedEnvelope(any()) }.returns(Unit)
         val trimmerMock = mockk<FeedEnvelopeTrimmer>()
         every { trimmerMock.trim(any()) }.returns(feedEnvelope)
-        val mergerMock = mockk<FeedEnvelopeMerger>()
 
-        val feedEnvelopeSaver = FeedEnvelopeSaver(persistenceMock, trimmerMock, mergerMock)
+        val feedEnvelopeSaver = FeedEnvelopeSaver(persistenceMock, trimmerMock)
 
         feedEnvelopeSaver.save(feedEnvelope)
 
         verifySequence {
             trimmerMock.trim(feedEnvelope)
             persistenceMock.saveFeedEnvelope(feedEnvelope)
-        }
-    }
-
-    @Test
-    fun `merge and save`() {
-        val feedEnvelope1 = FeedEnvelope(Source("source1", "address1"), Feed())
-        val feedEnvelope2 = FeedEnvelope(Source("source2", "address2"), Feed())
-        val persistenceMock = mockk<FeedPersistence>()
-        every { persistenceMock.saveFeedEnvelope(any()) }.returns(Unit)
-        val trimmerMock = mockk<FeedEnvelopeTrimmer>()
-        every { trimmerMock.trim(any()) }.returns(feedEnvelope2)
-        val mergerMock = mockk<FeedEnvelopeMerger>()
-        every { mergerMock.merge(any(), any()) }.returns(feedEnvelope2)
-
-        val feedEnvelopeSaver = FeedEnvelopeSaver(persistenceMock, trimmerMock, mergerMock)
-
-        feedEnvelopeSaver.mergeAndSave(feedEnvelope1, feedEnvelope2)
-
-        verifySequence {
-            mergerMock.merge(feedEnvelope1, feedEnvelope2)
-            trimmerMock.trim(feedEnvelope2)
-            persistenceMock.saveFeedEnvelope(feedEnvelope2)
         }
     }
 }
