6db8cc74abba12d6472a4e4c369109f6d7dbdf9c
rafi@guengel.ch <rafi@guengel.ch>
2019-10-24 09:25:19 +0200

    Align with rewrites

--- cli/src/main/kotlin/Cli.kt
+++ cli/src/main/kotlin/Cli.kt
@@ -1,41 +1,39 @@
 package ch.guengel.funnel.cli
 
-import ch.guengel.funnel.build.info.readBuildInfo
-import ch.guengel.funnel.common.serialize
-import ch.guengel.funnel.domain.Feed
-import ch.guengel.funnel.domain.FeedEnvelope
-import ch.guengel.funnel.domain.Source
-import ch.guengel.funnel.kafka.Constants
-import ch.guengel.funnel.kafka.Producer
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.findObject
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import data.Feed
+import data.FeedEnvelope
+import data.Source
+import funnel.build.info.readBuildInfo
+import kafka.Producer
+import kafka.feedEnvelopeDeleteTopic
+import kafka.feedEnvelopePersistTopic
 
 private const val defaultKafkaAddress = "localhost:9092"
-private const val PERSIST_TOPIC = "ch.guengel.funnel.persist.envelope"
-private const val DELETE_TOPIC = "ch.guengel.funnel.delete.envelope"
-
 private val buildInfo = readBuildInfo("/git.json")
 
-private fun makeFeed(sourceName: String, sourceAddress: String): FeedEnvelope {
+private fun makeFeedEnvelope(sourceName: String, sourceAddress: String): FeedEnvelope {
     val source = Source(sourceName, sourceAddress)
     val feed = Feed()
     return FeedEnvelope(source, feed)
 }
 
 private fun createFeed(sourceName: String, sourceAddress: String, kafkaAddress: String) {
-    val feed = makeFeed(sourceName, sourceAddress)
+    val feed = makeFeedEnvelope(sourceName, sourceAddress)
     Producer(kafkaAddress).use {
-        it.send(PERSIST_TOPIC, Constants.noKey, serialize(feed))
+        it.send(feedEnvelopePersistTopic, feed)
     }
 }
 
 private fun deleteFeed(sourceName: String, kafkaAddress: String) {
+    val feed = makeFeedEnvelope(sourceName, "not required")
     Producer(kafkaAddress).use {
-        it.send(DELETE_TOPIC, Constants.noKey, sourceName)
+        it.send(feedEnvelopeDeleteTopic, feed)
     }
 }
 
