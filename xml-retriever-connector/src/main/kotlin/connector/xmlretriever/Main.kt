b05a9cc25a6c0516f76d3eb28e0eceebcb8f01c9
rafi@guengel.ch <rafi@guengel.ch>
2019-10-25 13:14:24 +0200

    Expose info on all services

--- xml-retriever-connector/src/main/kotlin/connector/xmlretriever/Main.kt
+++ xml-retriever-connector/src/main/kotlin/connector/xmlretriever/Main.kt
@@ -1,32 +1,32 @@
 package ch.guengel.funnel.connector.xmlretriever
 
 import ch.guengel.funnel.build.logBuildInfo
-import ch.guengel.funnel.configuration.readConfiguration
 import ch.guengel.funnel.feed.logic.FeedEnvelopeRetriever
 import ch.guengel.funnel.retriever.xml.XmlRetriever
+import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import org.slf4j.LoggerFactory
-import java.util.concurrent.CountDownLatch
 
 private val logger = LoggerFactory.getLogger("retriever-connector")
 
-fun main() {
+fun main(args: Array<String>) {
     logBuildInfo(logger)
-    val configuration = readConfiguration(Configuration)
+
+    val environment = commandLineEnvironment(args)
 
     val feedEnvelopeRetriever = FeedEnvelopeRetriever(XmlRetriever())
+    val kafkaServer = environment.config.property("kafka.server").getString()
     val allFeedsConsumer = AllFeedsConsumer(
         feedEnvelopeRetriever,
-        configuration[Configuration.kafka]
+        kafkaServer
     )
 
     allFeedsConsumer.start()
 
-    val countDownLatch = CountDownLatch(1)
     Runtime.getRuntime().addShutdownHook(Thread {
         allFeedsConsumer.close()
-        countDownLatch.countDown()
     })
 
-    logger.info("Startup complete")
-    countDownLatch.await()
+    embeddedServer(Netty, environment).start(wait = true)
 }
